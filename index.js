c181654e6c43eb08e5a43bcba6014a852437d8c3
Ben <bsadick@gmail.com>
2017-12-23 23:44:57 +0900

    fixed ajax, added some functionality to boot

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var _ = require('lodash')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const app = express()
-
+let dwm = {}
 let data = JSON.parse(fs.readFileSync('data.json'))
 let korean_choice = _.sample(data.words)
 
@@ -15,16 +15,16 @@ function choose_a_new_word()
   korean_choice = _.sample(data.words)
 }
 
-function do_words_match (givenword, usersword)
+function do_words_match (givenword, usersword, keyshit)
 {
   if (givenword == usersword)
   {
     //pof means "pass or fail"
-    return {pof:"Nice job!!", back:"back?", repeat: false}
+    return {keyshit: keyshit, useranswer: usersword, chosenword: givenword, pof:"Good show!!", back:"back?", correct: true}
   }
   else
   {
-    return {pof: "Almost! Keep your eyes on the prize!", back:"try again?", repeat: true}
+    return {keyshit:keyshit, useranswer: usersword, chosenword: givenword, pof: "Almost! Keep your eyes on the prize!", back:"try again?", correct: false}
   }
 }
 
@@ -35,22 +35,28 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res)=>{
+  dwm = {}
   let word = korean_choice
   res.render('quizpage', {word: word})
 })
 
 app.post('/', (req, res, next)=>{
-  console.log("post request");
-  console.log(req.body.message[0]);
+  // console.log("post request");
+  // console.log(req.body.message[0]);
   let uw = req.body.message[0]
-  //dwm means do words match
-  let dwm = do_words_match(korean_choice.Korean, uw);
+  let keyshit = req.body.message[1]
+  // console.log("keys hit", keyshit);
+  // console.log("body", req.body);
+  //dwm means do words match, uw means userword
+  dwm = do_words_match(korean_choice.Korean, uw, keyshit);
   choose_a_new_word();
-  console.log(dwm);
-  
-  res.render('anscheck', {dwm, dwm})
-  console.log('shouldnot reach here');
+  // console.log(dwm);
+  console.log(dwm.correct);
+res.render('anscheck', {dwm: dwm})
 })
 
+app.get('/anscheck', (req, res)=>{
+  res.render('anscheck', {dwm: dwm})
+})
 
 var server = app.listen(process.env.PORT || 3000)
